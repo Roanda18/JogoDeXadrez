d61edf114397d2f8f16e831ead6b3c2c54a48267
Matheus Peixoto Matos <roanda18@gmail.com>
2024-12-17 10:41:14 -0300

    Criando classe posicao xadrez

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -7,18 +7,12 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro1 tabuleiro = new Tabuleiro1(8, 8);
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 9));
-                Tela.imprimirTabuleiro(tabuleiro);
-            }
-            catch(TabuleiroException ex) 
-            {
-                Console.WriteLine(ex.Message);
-            }
+
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
 
 
         }
---
+++ JogoDeXadrez/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,33 @@
+﻿using JogoDeXadrez.Tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDeXadrez.Xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+
+    
+}
