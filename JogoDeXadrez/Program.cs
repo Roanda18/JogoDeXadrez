a96b5d24909a505ad5f3a65f62e0394d6c2251cc
Matheus Peixoto Matos <roanda18@gmail.com>
2024-12-17 11:06:05 -0300

    melhorando a implementacao do metodo imprimirPeca

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -7,12 +7,19 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
+            Tabuleiro1 tab = new Tabuleiro1(8,8);
 
             PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
             Console.WriteLine(pos);
 
             Console.WriteLine(pos.toPosicao());
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(3, 3));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(6, 2));
+            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 1));
+
+            Tela.imprimirTabuleiro(tab);
 
 
         }
